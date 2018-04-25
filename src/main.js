30153b6d929005071a67ca5ddc46b53d49670fa1
Brenda Franco and Kayl Eubanks <brenda@>
2018-04-25 16:43:31 -0700

    Add logic to return only valid ingredients.

--- src/main.js
+++ src/main.js
@@ -18,50 +18,15 @@ $(document).ready(function(){
       responseImage.src=responseMeal.meals[0].strMealThumb;
       $("h4#name").text(responseMeal.meals[0].strMeal);
       $("p#instructions").text(responseMeal.meals[0].strInstructions);
-      // debugger;
-
 
       let meal = responseMeal.meals[0];
-      console.log(meal);
-      // let ingredientLists = Object.entries(meal);
-      // for (var i = 0; i < ingredientLists.length; i++) {
-      //   let ingredientKeyPairs = ingredientLists[i];
-      //   if ((ingredientKeyPairs[0].includes("strIngredient") && ingredientKeyPairs[1] !== "") || (ingredientKeyPairs[0].includes("strIngredient") && ingredientKeyPairs[1] !== null)) {
-      //     $("ul#ingredients").append(
-      //         `<li>${ingredientKeyPairs[1]}</li>`
-      //     );
-      //   }
-      // }
 
       for (let ingredient in meal) {
-        if (ingredient.includes("strIngredient") && meal[ingredient] !== "") {
+        if ( (ingredient.includes("strIngredient") && meal[ingredient] !== "") && (ingredient.includes("strIngredient") && meal[ingredient] !== null) ) {
           $("ul#ingredients").append(`<li>${meal[ingredient]}</li>`);
         }
       }
 
-
-
-      // responseMeal.meals[0].forEach(function(item) {
-      //   if (item.includes("strIngredient") {
-      //     $("ul#ingredients").append(`<li>${item}</li>`
-      //   }
-      // });
-
-
-      // for (var i = 0; i < 20; i++) {
-      //   let ingredientCheck = ".strIngredient" + i;
-      //   if (responseMeal.meals[0].) {
-      //
-      //   }
-        // let ingredientQuery = "responseMeal.meals[0].strIngredient" + i
-        // if (responseMeal.meals[0].strIngredient + i !== "" || responseMeal.meals[0].strIngredient + i !== null) {
-        //   let ingredient = responseMeal.meals[0].strIngredient + i;
-        //   console.log(ingredient);
-        //   $("ul#ingredients").append(
-        //     `<li>${ingredient}</li>`
-          // );
-      //   }
-      // }
       console.log("The response is: " + responseMeal.meals[0].strMeal);
       console.log("The response is: " + responseMeal.meals[0].strMealThumb);
       console.log("The response is: " + responseMeal.meals[0].strInstructions);
