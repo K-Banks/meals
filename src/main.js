06fd2ebdd695983cfade6ac53759be05eb030949
Brenda Franco and Kayl Eubanks <kayl@>
2018-04-26 15:33:06 -0700

    Add logic to return error when no results.

--- src/main.js
+++ src/main.js
@@ -49,47 +49,53 @@ $(document).ready(function(){
     let promiseMeals = ingredientCall.requestIngredientAPI(searchString);
 
     promiseMeals.then(function(responseMeals) {
-      responseMeals.meals.forEach(function(meal) {
-        $("div.wrapper").append(
-          `<div class="card thumbnail">
-            <span id="${meal.idMeal}" class="takeMeThere">
-              <img class="card-img-top image" src="${meal.strMealThumb}" alt="image of ${meal.strMeal}">
-              <h4 class="card-title name">${meal.strMeal}</h4>
-            </span>
-          </div>`
-        );
-        $("span.takeMeThere").last().on('click', function(event) {
-          event.preventDefault();
-          let id = this.id;
-          let idCall = new API();
-          let promiseID = idCall.requestIdAPI(id);
+      debugger;
+      if (responseMeals.meals === null) {
+        $("div.error").text("Your search returned no results.");
+        $("div.error").show();
+      } else {
+        responseMeals.meals.forEach(function(meal) {
+          $("div.wrapper").append(
+            `<div class="card thumbnail">
+              <span id="${meal.idMeal}" class="takeMeThere">
+                <img class="card-img-top image" src="${meal.strMealThumb}" alt="image of ${meal.strMeal}">
+                <h4 class="card-title name">${meal.strMeal}</h4>
+              </span>
+            </div>`
+          );
+          $("span.takeMeThere").last().on('click', function(event) {
+            event.preventDefault();
+            $("ul#ingredients-modal").empty();
+            let id = this.id;
+            let idCall = new API();
+            let promiseID = idCall.requestIdAPI(id);
 
-          promiseID.then(function(response) {
-            $(".recipe").show();
-            let responseMeal = response;
-            let meal = responseMeal.meals[0];
-            let ingredientArray = dataCall.ingredientCompiler(meal);
-            let measurementArray = dataCall.measurementCompiler(meal);
+            promiseID.then(function(response) {
+              $(".recipe").show();
+              let responseMeal = response;
+              let meal = responseMeal.meals[0];
+              let ingredientArray = dataCall.ingredientCompiler(meal);
+              let measurementArray = dataCall.measurementCompiler(meal);
 
-            let responseImage = document.getElementById("image");
-            responseImage.src=responseMeal.meals[0].strMealThumb;
-            $("h4#name").text(responseMeal.meals[0].strMeal);
-            $("p#instructions").text(responseMeal.meals[0].strInstructions);
+              let responseImage = document.getElementById("image-modal");
+              responseImage.src=responseMeal.meals[0].strMealThumb;
+              $("h4#name-modal").text(responseMeal.meals[0].strMeal);
+              $("p#instructions-modal").text(responseMeal.meals[0].strInstructions);
 
-            for (var i = 0; i < ingredientArray.length; i++) {
-              $("ul#ingredients").append(
-                `<li>${measurementArray[i]} ${ingredientArray[i]}</li>`
-              )
-            }
-          }, function(error) {
-            $("div.error").text("There has been an error: " + error);
-            $("div.error").show();
+              for (var i = 0; i < ingredientArray.length; i++) {
+                $("ul#ingredients-modal").append(
+                  `<li>${measurementArray[i]} ${ingredientArray[i]}</li>`
+                )
+              }
+            }, function(error) {
+              $("div.error").text("There has been an error: " + error);
+              $("div.error").show();
+            });
+            console.log("you clicked me!!");
+            console.log("This is the promise ID: " + promiseID);
           });
-          console.log("you clicked me!!");
-          console.log("This is the promise ID: " + promiseID);
         });
-
-      });
+      }
     }, function(error) {
       console.log("There was an error " + error);
     });
