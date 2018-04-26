0d7c5ba840179d843595e5c6632aa4462409146e
Brenda Franco and Kayl Eubanks <brenda@>
2018-04-26 14:50:14 -0700

    Add new API call for recipe ID.

--- src/api.js
+++ src/api.js
@@ -34,6 +34,24 @@ class API {
     return promise;
   }
 
+  requestIdAPI(searchString) {
+    let promise = new Promise(function(resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${searchString}`);
+      xhr.send();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          let response = JSON.parse(xhr.response);
+          resolve(response);
+        } else {
+          reject(Error(xhr.statusText));
+        }
+      }
+    });
+    return promise;
+  }
+
+
 }
 
 export { API };
