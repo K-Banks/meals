b3f5c209e6088693a2a404c4874cbfde653d398e
Brenda Franco and Kayl Eubanks <kayl@>
2018-04-26 09:25:57 -0700

    Add logic to append measurements with ingredients.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
   $("form#randomMeal").submit(function(event) {
     event.preventDefault();
     $("ul#ingredients").empty();
+    $(".recipe").show();
     let randomCall = new API();
     let promiseMeal = randomCall.requestRandomAPI();
 
@@ -20,13 +21,29 @@ $(document).ready(function(){
       $("p#instructions").text(responseMeal.meals[0].strInstructions);
 
       let meal = responseMeal.meals[0];
+      let ingredientArray = [];
+      let measurementArray = [];
 
       for (let ingredient in meal) {
         if ( (ingredient.includes("strIngredient") && meal[ingredient] !== "") && (ingredient.includes("strIngredient") && meal[ingredient] !== null) ) {
-          $("ul#ingredients").append(`<li>${meal[ingredient]}</li>`);
+          ingredientArray.push(meal[ingredient]);
         }
       }
 
+      for (let measurement in meal) {
+        if ( measurement.includes("strMeasure") ) {
+          measurementArray.push(meal[measurement]);
+        }
+      }
+
+      for (var i = 0; i < ingredientArray.length; i++) {
+        $("ul#ingredients").append(
+          `<li>${measurementArray[i]} ${ingredientArray[i]}</li>`
+        )
+      }
+
+      console.log("Ingredient Array" + ingredientArray);
+      console.log("Measurement Array" + measurementArray);
       console.log("The response is: " + responseMeal.meals[0].strMeal);
       console.log("The response is: " + responseMeal.meals[0].strMealThumb);
       console.log("The response is: " + responseMeal.meals[0].strInstructions);
